ddbc82f50b5f8d30ca9d966cb3c242e68edf8332
piyush8 <peyushchowhan7@gmail.com>
2018-09-13 19:54:56 +0530

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,20 @@
+# Load the Rails application.
+require_relative 'application'
+require 'whenever/capistrano'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'piyush8',
+  :password => 'jalaram4050',
+  :domain => 'localhost',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+set :whenever_command, "bundle exec whenever"
+set :whenever_environment, defer { stage }
+set :whenever_identifier, defer { "#{User}_#{development}" }
